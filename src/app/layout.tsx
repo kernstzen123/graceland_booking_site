a143aa28c10049a4d69af67c09593dd4f61b44f3
Khalid <ernstzenkhalid6@gmail.com>
2026-09-14 22:43:38 +0200

    added vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata, Viewport } from 'next'
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: 'Graceland Venues Booking',
@@ -43,6 +44,7 @@ export default function RootLayout({
           }}
         />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
